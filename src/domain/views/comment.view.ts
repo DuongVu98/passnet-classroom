3ef56ea45e89a5b8cce4da6298373df844e35bfa
Tung Duong <duongvutung1998@gmail.com>
2020-10-22 15:08:53 +0700

    save files

---
+++ src/domain/views/comment.view.ts
@@ -0,0 +1,4 @@
+export class CommentView {
+    commentId;
+    content;
+}
---
+++ src/domain/views/post.view.ts
@@ -0,0 +1,10 @@
+export class PostView {
+    postId: string;
+    content: string;
+}
+
+export class PostFullCommentsView {
+    postId: string;
+    content: string;
+    comments: string[];
+}
