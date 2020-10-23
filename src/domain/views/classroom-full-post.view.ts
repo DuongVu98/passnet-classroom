f5f32720f092d690ee1cdbbc344172514434ebf9
Tung Duong <duongvutung1998@gmail.com>
2020-10-23 17:34:14 +0700

    add views model

--- src/domain/views/classroom-full-post.view.ts
+++ src/domain/views/classroom-full-post.view.ts
@@ -1,3 +1,12 @@
-export class ClassroomFullPostsView {
-	classroomId: string;
+import { PostView } from "./post.view";
+import { StudentView } from "./student.view";
+import { TeacherView } from "./teacher.view";
+
+export class ClassroomView {
+    classroomId: string;
+    courseName: string;
+    students: StudentView[];
+    teacher: TeacherView;
+    teacherAssistances: StudentView[];
+    posts: PostView[];
 }
