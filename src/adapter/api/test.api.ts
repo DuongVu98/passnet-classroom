d829c2a278807ba9952dbfb1b5158a92fb3b586f
Tung Duong <duongvutung1998@gmail.com>
2020-10-25 14:36:23 +0700

    format code

--- src/adapter/api/test.api.ts
+++ src/adapter/api/test.api.ts
@@ -4,16 +4,16 @@ import { SomeService } from "src/usecases/some.service";
 
 @Controller("test")
 export class TestApi {
-    constructor(private someService: SomeService) {}
-    
+	constructor(private someService: SomeService) {}
+
 	@Get()
 	test(): void {
-        this.someService.someExecute();
-    }
-    
-    @UseInterceptors(LoggingInterceptor)
-    @Get("test2")
-    test2(): void {
-        return null;
-    }
+		this.someService.someExecute();
+	}
+
+	@UseInterceptors(LoggingInterceptor)
+	@Get("test2")
+	test2(): void {
+		return null;
+	}
 }
