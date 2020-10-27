5c906982e00d79be34e0a233ea886439d221dc59
Tung Duong <duongvutung1998@gmail.com>
2020-10-27 21:41:27 +0700

    add view editor for classroom

--- src/domain/views/classroom.view.ts
+++ src/domain/views/classroom.view.ts
@@ -1,3 +1,4 @@
+import { ClassroomAggregateRoot } from "../aggregate/classroom.aggregate";
 import { PostView } from "./post.view";
 import { StudentView } from "./student.view";
 import { TeacherView } from "./teacher.view";
@@ -9,4 +10,36 @@ export class ClassroomView {
 	teacher: TeacherView;
 	teacherAssistances: StudentView[];
 	posts: PostView[];
+}
+
+export class ClassroomViewEditor {
+	constructor(private view: ClassroomView) {}
+
+	withClassroomId(id: string): ClassroomViewEditor {
+		this.view.classroomId = id;
+		return this;
+	}
+	withCourseName(name: string): ClassroomViewEditor {
+		this.view.courseName = name;
+		return this;
+	}
+	withStudents(students: StudentView[]): ClassroomViewEditor {
+		this.view.students = students;
+		return this;
+	}
+	withTeacher(teacher: TeacherView): ClassroomViewEditor {
+		this.view.teacher = teacher;
+		return this;
+	}
+	withTeacherAssistances(tas: StudentView[]): ClassroomViewEditor {
+		this.view.teacherAssistances = tas;
+		return this;
+	}
+	withPosts(posts: PostView[]): ClassroomViewEditor {
+		this.view.posts = posts;
+		return this;
+	}
+	build(): ClassroomView {
+		return this.view;
+	}
 }
