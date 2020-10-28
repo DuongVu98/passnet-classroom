f7b0248fefecd9df95bce3bd3b2882657d49db4a
Tung Duong <duongvutung1998@gmail.com>
2020-10-28 15:12:02 +0700

    delete redundant files

--- src/domain/aggregate/aggregate.factory.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@nestjs/common";
-import { ClassroomAggregateRoot } from "./classroom.aggregate";
-
-@Injectable()
-export class AggregateFactory {
-	public getAggregateRoot(aggregateIdentifier: string): ClassroomAggregateRoot {
-		return null;
-	}
-}
