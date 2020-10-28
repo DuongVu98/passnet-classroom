04c75b369a4f10bc0ee4ecfdbc831a538e11dbd6
Tung Duong <duongvutung1998@gmail.com>
2020-10-28 16:32:12 +0700

    format code

--- src/domain/view-repo/classroom-query.repository.ts
+++ src/domain/view-repo/classroom-query.repository.ts
@@ -5,9 +5,9 @@ import { ClassroomView, ClassroomViewDto } from "../views/classroom.view";
 
 @Injectable()
 export class ClassroomQueryRepository {
-    constructor(@InjectModel("classroom_views") private viewModel: Model<ClassroomView>) {}
+	constructor(@InjectModel("classroom_views") private viewModel: Model<ClassroomView>) {}
 
-    async getClassroomAggregateRoot(aggregateRoot: string): Promise<ClassroomViewDto> {
+	async getClassroomAggregateRoot(aggregateRoot: string): Promise<ClassroomViewDto> {
 		return this.viewModel.aggregate([{ $match: { classroomId: aggregateRoot } }]).exec();
 	}
 }
