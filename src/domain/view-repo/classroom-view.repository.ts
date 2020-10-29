942f23bb43464cdaa633219bb7ddfab43d745569
Tung Duong <duongvutung1998@gmail.com>
2020-10-30 01:51:16 +0700

    modify error handler

--- src/domain/view-repo/classroom-view.repository.ts
+++ src/domain/view-repo/classroom-view.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable, Logger } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { ClassroomView, ClassroomViewDto } from "../views/classroom.view";
@@ -29,6 +29,10 @@ export class ClassroomViewRepository {
 			})
 			.then((updatedView) => {
 				view = updatedView;
+			})
+			.catch((error) => {
+				this.logger.error(`error when update view --> ${error}`);
+				throw new HttpException("view not found", HttpStatus.NOT_FOUND);
 			});
 
 		return view;
