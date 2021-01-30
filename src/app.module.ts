18ab5a3f855368263a6488e5fde860b290dc6957
Tony <duongvutung1998@gmail.com>
2021-01-30 16:22:46 +0700

    remove redundant imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { CacheInterceptor, CacheModule, Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { DomainModule } from "./domain/domain.module";
@@ -6,15 +6,9 @@ import { UsecasesModule } from "./usecases/usecases.module";
 import { AdapterModule } from "./adapter/adapter.module";
 import { AppConfigModule } from "./config/config.module";
 import { MongooseModule } from "@nestjs/mongoose";
-import * as redisStore from "cache-manager-redis-store";
 @Module({
 	imports: [
 		MongooseModule.forRoot("mongodb://localhost:27017/passnet_classroom_db"),
-		// CacheModule.register({
-		// 	store: redisStore,
-		// 	host: "192.168.99.100",
-		// 	port: 6379,
-		// }),
 		DomainModule,
 		UsecasesModule,
 		AdapterModule,
@@ -23,10 +17,6 @@ import * as redisStore from "cache-manager-redis-store";
 	controllers: [AppController],
 	providers: [
 		AppService,
-		// {
-		// 	provide: APP_INTERCEPTOR,
-		// 	useClass: CacheInterceptor,
-		// },
 	],
 })
 export class AppModule {}
