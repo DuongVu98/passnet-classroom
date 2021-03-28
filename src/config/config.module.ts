1f8b19f0df361ecc6297cd8addd4ccc634d1a3ef
DuongVu98 <duongvutung1998@gmail.com>
2021-03-29 04:55:08 +0700

    wip: try to resolve dependency conflict

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 
 const config = {
-	envFilePath: `src/env/.${process.env.NODE_ENV}.env`,
+	envFilePath: `${process.env.ENV_PATH}/.${process.env.NODE_ENV}.env`,
 };
 
 @Module({
